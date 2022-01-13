aa1b8c7ef991ef9e1f975de9b165769ec7d91960
Jason Yee <446031+jwsy@users.noreply.github.com>
2022-01-13 16:23:56 +0000

    Add fields for created/updated and done Update template with new fields

--- todo/admin.py
+++ todo/admin.py
@@ -3,9 +3,7 @@ from .models import TodoItem
 
 @admin.register(TodoItem)
 class TodoAdmin(admin.ModelAdmin):
-     list_display = ('content', 'owner')
-     list_filter = ('content', 'owner')
-
-# admin.site.register(TodoItem)
+    list_display = ('content', 'owner', 'done')
+    list_filter = ('content', 'owner', 'done')
 
 # Register your models here.
--- todo/models.py
+++ todo/models.py
@@ -2,5 +2,10 @@ from django.db import models
 
 # Create your models here.
 class TodoItem(models.Model):
-  content = models.TextField()
-  owner = models.TextField()
+    content = models.TextField()
+    owner = models.TextField(blank=True)
+    done = models.BooleanField(default=False)
+    # https://stackoverflow.com/questions/3429878/automatic-creation-date-for-django-model-form-objects
+    # I had to run a manual `python manage.py makemigrations` and provide a blank date
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True)
